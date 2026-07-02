cc4687a75c659cd9426c47d72e14be03e8c1fbbc
nytafar <lasse@nyta.no>
2026-07-02 19:58:35 +0200

    Strip debug output from the variation tiles template
    Remove the leftover <!-- Debug --> comments, the $GLOBALS/$variation_debug collectors, and the [INACTIVE] label suffix (keeping the inactive class hook) so none of it leaks into the rendered markup.

--- templates/single-product/add-to-cart/variable.php
+++ templates/single-product/add-to-cart/variable.php
@@ -21,16 +21,6 @@ $attribute_keys = array_keys($attributes);
 $variations_json = wp_json_encode($available_variations);
 $variations_attr = function_exists('wc_esc_json') ? wc_esc_json($variations_json) : _wp_specialchars($variations_json, ENT_QUOTES, 'UTF-8', true);
 
-// Debug information - safer approach
-if (is_array($available_variations) && !empty($available_variations)) {
-    $debug_first_variation = $available_variations[0];
-    // Store debug info in a global variable for inspection
-    $GLOBALS['wc_variation_debug'] = array(
-        'first_variation' => $debug_first_variation,
-        'attributes_sample' => isset($debug_first_variation['attributes']) ? $debug_first_variation['attributes'] : 'no_attributes'
-    );
-}
-
 do_action('woocommerce_before_add_to_cart_form'); ?>
 
 <form class="variations_form cart" action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint($product->get_id()); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
@@ -64,19 +54,9 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 								$valid_options = array();
 								$attr_key = 'attribute_' . $sanitized_name;
 								
-								// Debug info for variations
-								$variation_debug = array();
-								
 								if (is_array($available_variations)) {
 									// Check each variation
 									foreach ($available_variations as $index => $variation) {
-										// Track this variation for debugging
-										$variation_debug[$index] = array(
-											'purchasable' => isset($variation['is_purchasable']) ? $variation['is_purchasable'] : 'not_set',
-											'in_stock' => isset($variation['is_in_stock']) ? $variation['is_in_stock'] : 'not_set',
-											'attribute_value' => isset($variation['attributes'][$attr_key]) ? $variation['attributes'][$attr_key] : 'not_set'
-										);
-										
 										// Only consider variations that are purchasable and in stock
 										if (isset($variation['is_purchasable']) && $variation['is_purchasable'] && 
 											$variation['is_in_stock']) {
@@ -103,22 +83,6 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 									$valid_options = $options;
 								}
 								
-								// Store for debugging
-								if (!isset($GLOBALS['wc_variation_debug'])) {
-									$GLOBALS['wc_variation_debug'] = array();
-								}
-								$GLOBALS['wc_variation_debug']['attribute_' . $sanitized_name] = array(
-									'all_options' => $options,
-									'valid_options' => $valid_options,
-									'available_variations_count' => is_array($available_variations) ? count($available_variations) : 'not_array',
-									'has_any_value' => isset($has_any_value) ? $has_any_value : 'not_set'
-								);
-								
-								// Add a visible debug comment for this attribute
-								echo '<!-- Debug for ' . esc_html($name) . ': ' . 
-								     'Valid options: ' . esc_html(implode(', ', $valid_options)) . 
-								     ' | All options: ' . esc_html(implode(', ', $options)) . ' -->';
-								
 								if (taxonomy_exists($name)) {
 									// Get terms if this is a taxonomy - ordered. We need the names too.
 									$terms = wc_get_product_terms($product->get_id(), $name, array('fields' => 'all'));
@@ -130,8 +94,7 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 										}
 										// Check if option is in valid options
 										$is_active = in_array($term->slug, $valid_options);
-										// Add debug info to the term name to see active/inactive status
-										$debug_name = $term->name . ($is_active ? '' : ' [INACTIVE]');
+										$debug_name = $term->name;
 										// Pass additional class for inactive options
 										print_attribute_radio($checked_value, $term->slug, $debug_name, $sanitized_name, $is_active ? '' : 'inactive');
 									}
@@ -139,8 +102,7 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 									foreach ($options as $option) {
 										// Check if option is in valid options
 										$is_active = in_array($option, $valid_options);
-										// Add debug info to the option name to see active/inactive status
-										$debug_name = $option . ($is_active ? '' : ' [INACTIVE]');
+										$debug_name = $option;
 										// Pass additional class for inactive options
 										print_attribute_radio($checked_value, $option, $debug_name, $sanitized_name, $is_active ? '' : 'inactive');
 									}
@@ -188,9 +150,3 @@ do_action('woocommerce_before_add_to_cart_form'); ?>
 
 <?php do_action('woocommerce_after_add_to_cart_form'); ?>
 
-<?php
-// Output debug information if user is an admin
-if (current_user_can('manage_options') && isset($GLOBALS['wc_variation_debug'])) {
-    echo '<!-- Debug Info: ' . esc_html(json_encode($GLOBALS['wc_variation_debug'])) . ' -->';
-}
-?>
